6fff7bcae51b9c23e0e22b24598096d226393e44
GabrielaCavalcanti <gabrielacavalcanti6@gmail.com>
2022-04-06 11:49:44 -0300

    modified: arquivo js dataProdutos corrigido para mostrar imagens e arquivo js script modificado para resolucao da entrega

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,80 @@
+const ul = document.querySelector(".containerListaProdutos ul");
 
+function montarListaProdutos(listaProdutos) {
+
+    ul.innerHTML = "";
+
+      listaProdutos.forEach(element => {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+        const span = document.createElement('span');
+
+        img.src = element.img;
+        img.alt = element.nome;
+        h3.innerText = element.nome;
+        p.innerText = element.preco;
+        span.innerText = element.secao;
+
+        li.append(img, h3, p, span);
+
+        ul.appendChild(li)
+          
+      });  
+
+      atualizaPrecoTotal(listaProdutos)
+}
+
+montarListaProdutos(produtos)
+
+function filtrarPorHortifruti() {
+    
+    const prodsHortifruti = produtos.filter( elemento => {
+        return elemento.secao === "Hortifruti"
+    })
+
+    montarListaProdutos(prodsHortifruti)
+}
+
+const botaoFiltrarHortifruti = document.querySelector(".estiloGeralBotoes--filtrarHortifruti");
+
+botaoFiltrarHortifruti.addEventListener("click", filtrarPorHortifruti)
+
+function mostrarTodos(){
+    montarListaProdutos(produtos)
+}
+
+const botaoMostrarTodos = document.querySelector(".estiloGeralBotoes--mostrarTodos");
+
+botaoMostrarTodos.addEventListener("click", mostrarTodos)
+
+function filtrarCampoDeBusca(){
+
+    const input = document.querySelector(".campoBuscaPorNome")
+    const textoBusca = input.value;
+
+    const produtosBuscados = produtos.filter(element => {
+        return (element.nome).toUpperCase().includes(textoBusca.toUpperCase())
+    })
+
+    montarListaProdutos(produtosBuscados)
+
+}
+
+const botaoBusca = document.querySelector(".estiloGeralBotoes--botaoBuscaPorNome");
+
+botaoBusca.addEventListener("click", filtrarCampoDeBusca)
+
+function atualizaPrecoTotal(listaProdutos){
+
+    let precoTotalProdutos = 0;
+    const total = document.getElementById("precoTotal");
+
+    for(let i = 0; i < listaProdutos.length; i++){
+        precoTotalProdutos += listaProdutos[i].preco
+    }
+
+    total.innerText = precoTotalProdutos
+
+}
